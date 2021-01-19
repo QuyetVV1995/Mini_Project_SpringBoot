28cbfb5565a69c4568bde60c9b990557bb88f138
QuyetVV95 <quyeta2ubqn@gmail.com>
2021-01-19 17:32:43 +0700

    demo Blog: change properties ID from AUTO to IDENTITY

--- demoBlog/src/main/resources/sql/import-h2.sql
+++ demoBlog/src/main/resources/sql/import-h2.sql
@@ -3,23 +3,23 @@ ALTER TABLE POST
 
 -- Users
 -- password in plaintext: "password"
-INSERT INTO USER (user_id, password, email, username, name, last_name, active)
+INSERT INTO USER ( password, email, username, name, last_name, active)
 VALUES
-  (1, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'user@mail.com', 'user', 'Name', 'Surname',
+  ( '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'user@mail.com', 'user', 'Name', 'Surname',
    1);
 -- password in plaintext: "password"
-INSERT INTO USER (user_id, password, email, username, name, last_name, active)
+INSERT INTO USER ( password, email, username, name, last_name, active)
 VALUES
-  (2, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'johndoe@gmail.com', 'johndoe', 'John', 'Doe', 1);
+  ( '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'johndoe@gmail.com', 'johndoe', 'John', 'Doe', 1);
 -- password in plaintext: "password"
-INSERT INTO USER (user_id, password, email, username, name, last_name, active)
-VALUES (3, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'ana@mail.com', 'ana', 'Ana', 'Surname', 1);
+INSERT INTO USER ( password, email, username, name, last_name, active)
+VALUES ( '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'ana@mail.com', 'ana', 'Ana', 'Surname', 1);
 
 -- Roles
-INSERT INTO ROLE (role_id, role)
-VALUES (1, 'ROLE_ADMIN');
-INSERT INTO ROLE (role_id, role)
-VALUES (2, 'ROLE_USER');
+INSERT INTO ROLE ( role)
+VALUES ( 'ROLE_ADMIN');
+INSERT INTO ROLE ( role)
+VALUES ( 'ROLE_USER');
 
 -- User Roles
 INSERT INTO USER_ROLE (user_id, role_id)
@@ -32,79 +32,79 @@ INSERT INTO USER_ROLE (user_id, role_id)
 VALUES (3, 2);
 
 -- Posts
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (1, 1, 'Title 1',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 1, 'Title 1',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         --         CURRENT_TIMESTAMP());
         {ts '2016-10-19 11:10:13.247'});
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (2, 1, 'Title 2',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 1, 'Title 2',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         --         CURRENT_TIMESTAMP());
         {ts '2016-11-10 11:10:13.247'});
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (3, 1, 'Title 3',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 1, 'Title 3',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (4, 1, 'Title 4',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 1, 'Title 4',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (5, 1, 'Title 5',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 1, 'Title 5',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (6, 1, 'Title 6',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 1, 'Title 6',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (7, 2, 'Title 7',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 2, 'Title 7',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (8, 2, 'Title 8',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 2, 'Title 8',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (9, 2, 'Title 9',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 2, 'Title 9',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (10, 2, 'Title 10',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 2, 'Title 10',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (11, 3, 'Title 11',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 3, 'Title 11',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO POST (post_id, user_id, title, body, create_date)
-VALUES (12, 3, 'Title 12',
+INSERT INTO POST ( user_id, title, body, create_date)
+VALUES ( 3, 'Title 12',
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
 
 -- Comments
-INSERT INTO COMMENT (comment_id,post_id, user_id, body, create_date)
-VALUES (1,1, 1,
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (1, 1,
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO COMMENT (comment_id,post_id, user_id, body, create_date)
-VALUES (2,1, 2,
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (1, 2,
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO COMMENT (comment_id,post_id, user_id, body, create_date)
-VALUES (3,1, 3,
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (1, 3,
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO COMMENT (comment_id,post_id, user_id, body, create_date)
-VALUES (4,6, 1,
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (6, 1,
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO COMMENT (comment_id,post_id, user_id, body, create_date)
-VALUES (5,6, 2,
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (6, 2,
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
-INSERT INTO COMMENT (comment_id,post_id, user_id, body, create_date)
-VALUES (6,6, 3,
+INSERT INTO COMMENT (post_id, user_id, body, create_date)
+VALUES (6, 3,
         '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
         CURRENT_TIMESTAMP());
