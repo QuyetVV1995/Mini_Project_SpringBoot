e8753add5aac98b99920a020985a63d18f5ed419
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-04-07 16:57:16 +0700

    Ecommerce

---
+++ Ecommerce/devdacticShop/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAYcY7Lb_vJ6uJJlgQ3fTAKR2cW_LEmQgQ",
+    authDomain: "ecommerce-fd89b.firebaseapp.com",
+    projectId: "ecommerce-fd89b",
+    storageBucket: "ecommerce-fd89b.appspot.com",
+    messagingSenderId: "876667331836",
+    appId: "1:876667331836:web:9a610da92e03dc3296e379",
+    measurementId: "G-XVG0WK3VTL"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
