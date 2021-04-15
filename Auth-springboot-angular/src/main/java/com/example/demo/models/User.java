b73b561cb37812ab0d08d899638b13f94395acf4
QuyetVV1995 <quyeta2ubqn@gmail.com>
2021-04-15 10:22:30 +0700

    get edit delete post by admin BE

--- Auth-springboot-angular/src/main/java/com/example/demo/models/User.java
+++ Auth-springboot-angular/src/main/java/com/example/demo/models/User.java
@@ -80,6 +80,7 @@ public class User {
     @JoinTable(	name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JsonIgnore
     private Set<Role> roles = new HashSet<>();
 
     //Một User viết nhiều Post
